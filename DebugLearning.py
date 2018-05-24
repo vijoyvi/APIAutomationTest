ad8ed0a6307bcf03e64e2b7a40e7bb00685c6745
vijoyvi <39193858+vijoyvi@users.noreply.github.com>
2018-05-24 19:50:44 +0530

    Update DebugLearning.py

--- DebugLearning.py
+++ DebugLearning.py
@@ -5,11 +5,13 @@ class Solver:
 
     def demo(self, a, b, c):
         print "Testing the Version Control System Options"
+        print "Adding a line from the Git HUb"
         d = b ** 2 - 4 * a * c
         if d > 0:
             disc = math.sqrt(d)
             root1 = (-b + disc) / (2 * a)
             root2 = (-b - disc) / (2 * a)
+            print "Adding a second line from the Git HUb"
             return root1, root2
         elif d == 0:
             return -b / (2 * a)
